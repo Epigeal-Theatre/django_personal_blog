3d13337bb3e84f40db88d7491d41fa1d618bbb4c
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-10-18 04:07:00 +0300

    epigeal-theatre

---
+++ django_project/users/urls.py
@@ -0,0 +1,12 @@
+'''
+this should be the standard way of making urls then pasing them to...
+the project scope urls.py but we hold off for now
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('', views.home, name='blog-home')
+
+'''
