f9cdc7a35a2420195eee4afaf2807b0850a53d74
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-10-19 23:36:28 +0300

    configure gmail settings for password resets and other functions

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -5,20 +5,20 @@ from PIL import Image
 
 
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-	def __str__(self):
-		return f'{self.user.username} Profile'
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
+    def save(self, *args, **kwargs):  # Add *args and **kwargs here
+        super().save(*args, **kwargs)  # Pass *args and **kwargs to the parent method
 
-	def save(self):
-		super().save()
+        img = Image.open(self.image.path)
 
-		img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
-		if img.height > 300 or img.width > 300:
-			output_size = (300,300)
-			img.thumbnail(output_size)
-			img.save(self.image.path)
 
