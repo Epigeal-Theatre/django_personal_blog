36a8230a81c25e6ccd9fb0fac1edb3c435f7064b
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-10-21 13:10:20 +0300

    comment users/views.py

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -3,42 +3,41 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
-
+# View for user registration
 def register(request):
-	if request.method == 'POST':
-		form =  UserRegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			messages.success(request, f'Your Account Has Been Created. You Are Now Able To Log In')
-			return redirect('login')
-	else:
-		form =UserRegisterForm()
-	return render(request, 'users/register.html', {'form': form})
+    if request.method == 'POST':  # If the form is submitted via POST
+        form = UserRegisterForm(request.POST)  # Create a form instance with the submitted data
+        if form.is_valid():  # Validate the form
+            form.save()  # Save the user to the database
+            username = form.cleaned_data.get('username')  # Get the username from the form
+            messages.success(request, f'Your account has been created! You are now able to log in.')  # Success message
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserRegisterForm()  # If not POST, create an empty form for a GET request
+    return render(request, 'users/register.html', {'form': form})  # Render the registration page with the form
 
 
+# View for updating user profile (requires login)
 @login_required
 def profile(request):
-	if request.method == 'POST':
-		u_form = UserUpdateForm(request.POST, instance=request.user)
-		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    if request.method == 'POST':  # If the form is submitted via POST
+        u_form = UserUpdateForm(request.POST, instance=request.user)  # Form to update user details
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)  # Form to update profile
 
-		if u_form.is_valid() and p_form.is_valid():
-			u_form.save()
-			p_form.save()
-			messages.success(request, f'Your Acoount Has Been Updated')
-			return redirect('profile')
-	else:
-		u_form = UserUpdateForm(instance=request.user)
-		p_form = ProfileUpdateForm(instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():  # Validate both forms
+            u_form.save()  # Save updated user data
+            p_form.save()  # Save updated profile data
+            messages.success(request, f'Your account has been updated!')  # Success message
+            return redirect('profile')  # Redirect to the profile page after saving changes
+    else:
+        u_form = UserUpdateForm(instance=request.user)  # Populate the form with the user's current data for GET
+        p_form = ProfileUpdateForm(instance=request.user.profile)  # Populate the profile form with current profile data
 
+    context = {
+        'u_form': u_form,  # Pass the user update form to the template
+        'p_form': p_form   # Pass the profile update form to the template
+    }
 
-	context = {
-		'u_form': u_form,
-		'p_form': p_form
-	}
+    return render(request, 'users/profile.html', context)  # Render the profile page with both forms
 
-
-	return render(request, 'users/profile.html', context)
 
