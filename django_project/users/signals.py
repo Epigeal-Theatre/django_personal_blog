7e6cd31fdef68a691454a5d51e2c7e874ff3f390
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-10-21 13:08:03 +0300

    comment signals.py

--- django_project/users/signals.py
+++ django_project/users/signals.py
@@ -3,14 +3,13 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
+# Signal to automatically create a Profile whenever a new User is created.
+@receiver(post_save, sender=User)  # Connect the post_save signal to this function for the User model.
+def create_profile(sender, instance, created, **kwargs):
+    if created:  # If a new user is created, create a corresponding Profile.
+        Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance,created, **kwargs):
-	if created:
-		Profile.objects.create(user=instance)
-
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance,created, **kwargs):
-	instance.profile.save()
+# Signal to save the Profile whenever the User instance is saved.
+@receiver(post_save, sender=User)  # Connect the post_save signal to this function for the User model.
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()  # Save the related Profile whenever the User is saved.
