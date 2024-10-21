e95ac6e5bee9383f8d1a93e6f58f7d12eba65ea2
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-10-21 13:06:19 +0300

    comment users/models.py

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-# Create your models here.
 
-
+# Profile model to store additional user information, specifically their profile image.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Links each profile to one user (one-to-one relationship).
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')  # Stores profile image with a default image and upload location.
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username} Profile'  # Displays the profile as the user's username followed by "Profile".
 
-    def save(self, *args, **kwargs):  # Add *args and **kwargs here
-        super().save(*args, **kwargs)  # Pass *args and **kwargs to the parent method
+    # Override the save method to resize images before saving.
+    def save(self, *args, **kwargs):
+        # Call the parent class's save method to save the model first.
+        super().save(*args, **kwargs)
 
+        # Open the uploaded image using PIL.
         img = Image.open(self.image.path)
 
+        # Check if the image dimensions exceed 300x300 pixels.
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.thumbnail(output_size)  # Resize the image while maintaining aspect ratio.
+            img.save(self.image.path)  # Save the resized image back to the same path.
 
 
